97cd10d1a92ee2de38fcf7f687aef8d6235e12ff
yasolo <yasolo@yandex-team.ru>
2022-04-23 01:17:32 +0300

    Updates

---
+++ precache-manifest.ae48f1d3e9203e1a7fe90ef0aa166c61.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b14e7e363cdcad85a74",
+    "url": "/ProcessMiningVisualization/static/css/main.1f62a142.chunk.css"
+  },
+  {
+    "revision": "4b14e7e363cdcad85a74",
+    "url": "/ProcessMiningVisualization/static/js/main.4b14e7e3.chunk.js"
+  },
+  {
+    "revision": "9a56c5efe68dcaeb9b05",
+    "url": "/ProcessMiningVisualization/static/css/1.3f14477f.chunk.css"
+  },
+  {
+    "revision": "9a56c5efe68dcaeb9b05",
+    "url": "/ProcessMiningVisualization/static/js/1.9a56c5ef.chunk.js"
+  },
+  {
+    "revision": "393c27fc8b12b00a4e0c",
+    "url": "/ProcessMiningVisualization/static/js/runtime~main.393c27fc.js"
+  },
+  {
+    "revision": "a3314b8b9e773167a63e3c6b61d673c4",
+    "url": "/ProcessMiningVisualization/static/media/one-token.a3314b8b.svg"
+  },
+  {
+    "revision": "8a98c32b3383cffd120c48438d580802",
+    "url": "/ProcessMiningVisualization/static/media/two-tokens.8a98c32b.svg"
+  },
+  {
+    "revision": "0ebc35c3b5de7e8fdd8c9488b919edf1",
+    "url": "/ProcessMiningVisualization/static/media/three-tokens.0ebc35c3.svg"
+  },
+  {
+    "revision": "da8b9d752e2236968c4a1bfeff593138",
+    "url": "/ProcessMiningVisualization/static/media/four-tokens.da8b9d75.svg"
+  },
+  {
+    "revision": "7240809ba954d82a8569122e771c97c2",
+    "url": "/ProcessMiningVisualization/static/media/five-tokens.7240809b.svg"
+  },
+  {
+    "revision": "9b88cbc087c587f5446728ebed895c9d",
+    "url": "/ProcessMiningVisualization/static/media/six-tokens.9b88cbc0.svg"
+  },
+  {
+    "revision": "d7817d9b819fb63e65bea73f40b37b68",
+    "url": "/ProcessMiningVisualization/static/media/seven-tokens.d7817d9b.svg"
+  },
+  {
+    "revision": "a52db80b6001e50aa425cb3f2939a1ad",
+    "url": "/ProcessMiningVisualization/static/media/eight-tokens.a52db80b.svg"
+  },
+  {
+    "revision": "e2e95f09914fc87655617d289b46eb46",
+    "url": "/ProcessMiningVisualization/static/media/nine-tokens.e2e95f09.svg"
+  },
+  {
+    "revision": "4e973fa7990c9dea19f5dbc345fbe05a",
+    "url": "/ProcessMiningVisualization/index.html"
+  }
+];
